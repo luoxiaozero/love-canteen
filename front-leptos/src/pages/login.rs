fdd81a3f4b4ae31ed9a42439926daa5646d8615a
luoxiao <luoxiaozero@163.com>
2023-05-30 23:12:13 +0800

    ✨ feat: login

--- front-leptos/src/pages/login.rs
+++ front-leptos/src/pages/login.rs
@@ -1,37 +1,51 @@
 use crate::api::login::*;
 use leptos::*;
+use leptos_router::use_navigate;
 use melt_ui::*;
 
 #[component]
 pub fn Login(cx: Scope) -> impl IntoView {
     let account = create_rw_signal(cx, String::new());
     let password = create_rw_signal(cx, String::new());
+    let error_text = create_rw_signal(cx, String::new());
 
     let login = move |_| {
-        login_api(
-            LoginData {
-                account: account.get_untracked(),
-                password: password.get_untracked(),
-            },
-            |v| {
-                log!("login s");
-                match  v {
-                    Ok(data) => log!("{}", data.token),
-                    Err(err) => log!("{err}"),
-                };
-            },
-        );
+        let account = account.get_untracked();
+        if account.is_empty() {
+            error_text.set(String::from("账号不能为空"));
+            return;
+        }
+        let password = password.get_untracked();
+
+        if password.is_empty() {
+            error_text.set(String::from("密码不能为空"));
+            return;
+        }
+        login_api(LoginData { account, password }, move |v| {
+            log!("login s");
+            match v {
+                Ok(_data) => {
+                    let navigate = use_navigate(cx);
+                    _ = navigate("/", Default::default());
+                }
+                Err(err) => {
+                    error_text.set(err);
+                }
+            };
+        });
     };
 
     view! { cx,
         <div class="text-center p-2">"登录"</div>
         <div class="p-2">
-            <Input value=account on_input=account.into() />
+            <Input value=account />
         </div>
         <div class="p-2">
-            <Input value=password on_input=password.into()/>
+            <Input value=password type_="password"/>
+        </div>
+        <div class="p-1" style="color: red">
+            { move || error_text.get() }
         </div>
         <Button on:click=login>"登录"</Button>
     }
 }
-
