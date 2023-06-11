28a68896f7fbfd47d7a9fc7b5ef559de746ecea2
luoxiao <luoxiaozero@163.com>
2023-06-11 15:59:04 +0800

    🐞 fix: user order

--- back-juri/src/user/mod.rs
+++ back-juri/src/user/mod.rs
@@ -6,7 +6,7 @@ use juri::Router;
 pub fn router() -> Router {
     let mut router = Router::new();
     router.at("/user/login").post(login::login);
-    router.at("/user/order").post(order::get_order);
-    router.at("/user/order/get").post(order::add_order);
+    router.at("/user/order").get(order::get_order);
+    router.at("/user/order/add").post(order::add_order);
     router
 }
